f587d40c00c74b23ccc024e46258c2aae63e8f77
katsuraku <kirsten.jones@ft.com>
2017-02-04 12:22:43 +0000

    Separate kitty stuff into separate package, import and refer to it in main

--- main.go
+++ main.go
@@ -2,41 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/katsuraku/wwg/animals"
 )
 
 // can only have one func main in a Go program
 // it is the entry point - the thing that will run
-
-// BUT WHY USE POINTERS?
-// YOU DONT ALWAYS WANT TO BE MAKING COPIES OF THINGS
-
-type Kitten struct {
-	Name string
-}
-
-func (k Kitten) SetName(name string) {
-	k.Name = name
-}
-
-func (k Kitten) GetName() string {
-	return k.Name
-}
-
-//  NOW K IS JUST A REFERENCE TO KITTY, NOT A COPY OF IT
-// WITH THE ABOVE ONES, YOU GET A NEW K EVERY TIME. WITHOUT THE *, WHEN YOU CALL GETNAME YOU ARE ACTUALLY GETTING A BRAND
-// NEW COPY OF KITTY.
-// YOU DON'T NEED TO WORRY ABOUT THIS WITH TRUE OBJECT ORIENTED LANGUAGES
-// IN GO YOU'RE ACTUALLY ATTACHING AN OBJECT TO A METHOD
-// REMEMBER THE DIFFERENCE BETWEEN HAVING A STAR AND NOT HAVING A * IS THE DIFFERENCE BETWEEN HAVING A COPY AND A REFERENCE.
-// WITH A REFERENCE YOU CAN CHANGE/MUTATE 'NAME'
-// WITH A COPY YOURE NOT MUTATING THE SAME INSTANCE THAT YOU THINK YOU ARE.
-func (k *Kitten) SetNameRef(name string) {
-	k.Name = name
-}
-
-func (k *Kitten) GetNameRef() string {
-	return k.Name
-}
 
 func main() {
 	fmt.Println("yo")
@@ -61,7 +32,7 @@ func main() {
 	fmt.Println("d, dereferenced using * is ", *d)                      // dereferenced - looks through the pointer and gives the value when you look wherever the address is pointing you to.
 
 	// BUT WHY USE POINTERS? CHECK THIS OUT
-	kitty := Kitten{}
+	kitty := animals.Kitten{}
 	kitty.SetName("Mr Tiggles")
 	fmt.Println(kitty.GetName()) // THIS PRINTS NOTHING. WHY?
 	kitty.SetNameRef("Mr Someone")
